4b96c5e5fe2da679f199daf3aeb972280169d49b
willfrid boris <boriskwayep35@yahoo.com>
2020-12-07 00:12:44 -0500

    pushpin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '29ee209e955319b9fc9b19a060366ddca6e718a1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '29ee209e955319b9fc9b19a060366ddca6e718a1',
+    ),
+  ),
+);
